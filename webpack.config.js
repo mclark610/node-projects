5738db9a949f1ea2c611ff9d8e7ebf62a3819483
mclark <mclark610@yahoo.com>
2019-02-10 20:05:30 -0500

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,18 @@
 
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack')
 // node : the listed modules are built in to node.  webpack gets confused if
 //        they arent included in node
 // externals: webpack throws lots of errors for modules used in node_modules
 //            directory.  This is one way of telling webpack to not bother
 module.exports = {
     mode: 'development',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
     entry: {
         app: './server.js',
     },
@@ -23,7 +28,6 @@ module.exports = {
         tls: 'empty',
         net: 'empty'
     },
-
     output: {
         filename: 'server.bundle.js',
         path: path.resolve(__dirname,'dist'),
