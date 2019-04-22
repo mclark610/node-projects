bbdbb0fcf4c7ea03b0e83149594cdcb5a509e436
mclark <mclark610@yahoo.com>
2019-04-22 11:50:11 -0400

    minor cleanup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
+
 let component = () => {
     let element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello','webpack'], '');
 
     return element;
-}
+};
 
 document.body.appendChild(component());
